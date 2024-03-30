fb19b15deccb0666f93f4e317737be5c969ce02a
you@example.com <you@example.com>
2024-03-30 15:55:13 +1100

    v2

--- src/currency/DTO/queryParamsDTO.ts
+++ src/currency/DTO/queryParamsDTO.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsOptional, IsString } from "class-validator";
 
 export class QueryParamsDTO {
+    @ApiProperty()
     @IsString()
     from: string
 
+    @ApiProperty()
     @IsOptional()
     @IsString()
     to: string
 
+    @ApiProperty()
     @IsOptional()
     @IsString()
     amount: string
